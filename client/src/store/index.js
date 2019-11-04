be60627567c682a819f641f8a6dfacecf0bed353
junilhwang <tjsdlf4261@naver.com>
2019-11-04 15:17:02 +0900

    기본 scss 추가 완료

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import state from './state';
+import mutations from './mutations';
+import actions from './actions';
+import modules from './modules';
+
 Vue.use(Vuex);
 
+console.log(state);
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+  state,
+  mutations,
+  actions,
+  modules,
 });
