afba8079c851bf48e2a739640bfd278b6271d19f
junil-hwang <tjsdlf4261@naver.com>
2019-11-04 23:30:32 +0900

    메인페이지 네비게이션 완성

--- client/src/views/Main.vue
+++ client/src/views/Main.vue
@@ -1,7 +1,41 @@
 <template>
   <div class="content-wrap">
-    <section class="main-content">
-      <h2 class="main-content__title">News</h2>
+    <section>
+      <header class="content-header">
+        <ul>
+          <li v-for="(v, k) in menu" :key="k">
+            <a href="#" :class="{active: k === menuActive}" @click.prevent="activeMenu(k)">
+              <span v-html="v.title" />
+            </a>
+          </li>
+        </ul>
+      </header>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          title: '최신뉴스',
+        },
+        {
+          title: '음원차트',
+        },
+        {
+          title: '인기영상',
+        },
+      ],
+      menuActive: 0,
+    };
+  },
+  methods: {
+    activeMenu(menuIndex) {
+      this.menuActive = menuIndex;
+    },
+  },
+};
+</script>
