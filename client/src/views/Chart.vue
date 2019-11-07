9e078a2bfee0b9ab38d19e54158f0b70460cd7e9
junilhwang <tjsdlf4261@naver.com>
2019-11-07 16:59:30 +0900

    infinite scrll 적용 완료

--- client/src/views/Chart.vue
+++ client/src/views/Chart.vue
@@ -4,10 +4,10 @@
       <strong>비디오 영역입니다.</strong>
     </section>
     <section class="chart__wrap">
-      <article class="chart__article" v-for="k in 10" :key="k">
-        <strong class="chart__rank" v-html="k" />
+      <article class="chart__article" v-for="(v, k) in list" :key="k">
+        <strong class="chart__rank" v-html="k + 1" />
         <dl class="chart__info">
-          <dt>Music title {{k}}</dt>
+          <dt v-html="v" />
           <dd>Artist {{k}}</dd>
         </dl>
         <figure class="chart__thumbnail">
@@ -17,3 +17,40 @@
     </section>
   </main>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      initList: [
+        'Lorem ipsum dolor.',
+        'At, excepturi, fuga?',
+        'Est, obcaecati ratione.',
+        'Accusamus, aperiam, optio!',
+        'Natus, provident quae!',
+        'At eaque, tenetur.',
+        'Cupiditate totam, unde!',
+        'Adipisci laboriosam, rerum.',
+        'Blanditiis, cum incidunt?',
+        'Dolore, ipsam porro.',
+      ],
+      list: [],
+    };
+  },
+  created() {
+    this.list.push(...this.initList);
+    window.addEventListener('scroll', this.listLoading);
+  },
+  methods: {
+    listLoading() {
+      const { innerHeight, scrollY } = window;
+      const { scrollHeight } = document.body;
+      if (scrollHeight - innerHeight - scrollY === 0) {
+        this.list.push(...this.initList);
+        if (this.list.length >= 100) {
+          window.removeEventListener('scroll', this.listLoading);
+        }
+      }
+    },
+  },
+};
+</script>
