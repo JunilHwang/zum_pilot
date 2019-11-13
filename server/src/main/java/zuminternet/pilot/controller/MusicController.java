d83dcc6f6f1b865bac2d9d86c7afa5a02343b5d3
junilHwang <tjsdlf4261@naver.com>
2019-11-13 16:42:50 +0900

    Youtube + Google Service API 호출 완료

--- server/src/main/java/zuminternet/pilot/controller/MusicController.java
+++ server/src/main/java/zuminternet/pilot/controller/MusicController.java
@@ -2,10 +2,14 @@ package zuminternet.pilot.controller;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import zuminternet.pilot.domain.MusicArticle;
 import zuminternet.pilot.helper.Crawler;
+import zuminternet.pilot.helper.YoutubeSearch;
 
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class MusicController {
@@ -15,6 +19,16 @@ public class MusicController {
     HashMap send = new HashMap();
     send.put("success", true);
     send.put("articles", Crawler.getMusicList());
+    return send;
+  }
+
+  @GetMapping(value="/api/music-video")
+  public HashMap getMusicVideoList (@RequestParam(required = true) String q) {
+    HashMap send = new HashMap();
+    YoutubeSearch youtubeSearch = new YoutubeSearch();
+    List result = youtubeSearch.search(q);
+    send.put("success", true);
+    send.put("result", result);
     return send;
   }
 }
--- server/src/main/java/zuminternet/pilot/helper/YoutubeSearch.java
+++ server/src/main/java/zuminternet/pilot/helper/YoutubeSearch.java
@@ -1,31 +1,42 @@
 package zuminternet.pilot.helper;
 
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.SearchListResponse;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.util.List;
 
 public class YoutubeSearch {
-  public String search(String q) {
-    String result = "";
-    try {
-      String apiurl = "https://www.googleapis.com/youtube/v3/search"
-        + "?key=AIzaSyB9-VhCcc2neB2aYgVxJPZLtkb_jmfNo1o"
-        + "&part=snippet&q=" + URLEncoder.encode(q, "UTF-8");
-      URL url = new URL(apiurl);
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
+  private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+  private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+  private static final long NUMBER_OF_VIDEOS_RETURNED = 1;
+  private static YouTube youtube;
+  private static final String API_KEY = "AIzaSyB9-VhCcc2neB2aYgVxJPZLtkb_jmfNo1o";
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-      String inputLine;
-      StringBuffer response = new StringBuffer();
-      while ((inputLine = br.readLine()) != null) {
-        response.append(inputLine);
-      }
-      br.close();
-      result = response.toString();
+  public List search (String q) {
+    youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+      public void initialize(HttpRequest request) throws IOException {}
+    }).setApplicationName("youtube-cmdline-search-sample").build();
+    YouTube.Search.List search;
+    SearchListResponse searchResponse;
+    List result = null;
+    try {
+      BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+      search = youtube.search().list("snippet");
+      search.setKey(API_KEY);
+      search.setQ(q);
+      search.setType("video");
+      search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+      searchResponse = search.execute();
+      result = searchResponse.getItems();
     } catch (Exception e) {
       e.printStackTrace();
     }
