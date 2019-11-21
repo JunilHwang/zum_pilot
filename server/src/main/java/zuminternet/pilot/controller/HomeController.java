31d7983a8bd52d54dd21cce7a18d9c3deccbf90f
junilHwang <tjsdlf4261@naver.com>
2019-11-21 09:02:46 +0900

    작업중

--- server/src/main/java/zuminternet/pilot/controller/HomeController.java
+++ server/src/main/java/zuminternet/pilot/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  @GetMapping("/")
+  @GetMapping(value={"/", "/chart", "/popular"})
   public String home () {
-    return "home";
+    return "index";
   }
 }
