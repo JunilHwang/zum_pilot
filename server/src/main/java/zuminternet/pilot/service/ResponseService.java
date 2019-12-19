7e85fc9420b44613d61456566b0ce56cf69567cc
junilhwang <tjsdlf4261@naver.com>
2019-12-20 08:15:49 +0900

    Response Service 주석 ì삽입 완료

--- server/src/main/java/zuminternet/pilot/service/ResponseService.java
+++ server/src/main/java/zuminternet/pilot/service/ResponseService.java
@@ -12,7 +12,7 @@ public class ResponseService {
    * 단일 데이터를 받아온 후 Response를 반환
    * @param result : 반환할 단일 데이터 정보
    * @param <T> : 단일 데이터의 Type
-   * @return success 정보들 + 단일 데이터
+   * @return { msg, code, success, result }
    */
   public <T> SingleResult<T> singleResult (T result) {
     SingleResult<T> response = new SingleResult();
@@ -25,7 +25,7 @@ public class ResponseService {
    * 다중 데이터를 받아온 후 Response를 반한
    * @param result : 반환할 다중 데이터 정보
    * @param <T> : 각 데이터의 Type
-   * @return success 정보들 + 다중 데이터
+   * @return { msg, code, success, result }
    */
   public <T>ListResult<T> listResult (List<T> result) {
     ListResult<T> response = new ListResult();
@@ -34,12 +34,21 @@ public class ResponseService {
     return response;
   }
 
+  /**
+   * 기본적인 Response를 반환
+   * @return { msg, code, success }
+   */
   public CommonResult commonResult() {
     CommonResult response = new CommonResult();
     success(response);
     return response;
   }
 
+  /**
+   * Fail Response를 반환
+   * @param commonResponse : Fail에 대한 정보. FAIL|AUTH_FAIL|SIGN_IN_FAIL|SIGN_UP_FAIL 등
+   * @return { msg, code, success }
+   */
   public CommonResult failResult(CommonResponse commonResponse) {
     CommonResult response = new CommonResult();
     response.setSuccess(false);
@@ -48,6 +57,10 @@ public class ResponseService {
     return response;
   }
 
+  /**
+   * response에 기본적인 성공 코드를 삽입
+   * @param response
+   */
   private void success(CommonResult response) {
     response.setSuccess(true);
     response.setCode(CommonResponse.SUCCESS.getCode());
