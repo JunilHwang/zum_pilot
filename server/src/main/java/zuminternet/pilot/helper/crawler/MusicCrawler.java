3a12b8441554aec705d5bd6b42246df89b8a5df9
junilhwang <tjsdlf4261@naver.com>
2019-12-19 16:11:33 +0900

    Crawler 주석 추가 완료

--- server/src/main/java/zuminternet/pilot/helper/crawler/MusicCrawler.java
+++ server/src/main/java/zuminternet/pilot/helper/crawler/MusicCrawler.java
@@ -10,16 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class MusicCrawler extends Crawler {
+public class MusicCrawler extends Crawler { // Crawler Class를 Extends함 --> getDoc Method 사용 가능
 
   @Value("${url.melon}")
-  private String melonURL;
+  private String melonURL; // application.yml에 정의한 url.melon
 
   @Value("${uri.melon.genre}")
-  private String melonGenreURI;
+  private String melonGenreURI; // application.yml에 정의한 uri.melon.genre
 
+  /**
+   * 멜론에서 음원 차트 데이터를 가져옴
+   * @param category: 일간|발라드|댄스|히합|R&B/Soule|POP 등의 카테고리 데이터
+   * @return 음원 차트를 Music Article에 mapping해서 List로 반환
+   */
   public List<MusicArticle> getList (String category) {
+    // 기본으로 사용할 URI 정보
     String appendURI = "/chart/";
+
+    // Category에 따라서 URI를 선택
     switch (category) {
       case "일간" : appendURI += "day/"; break;
       case "발라드" : appendURI = melonGenreURI + "0100";  break;
@@ -28,11 +36,21 @@ public class MusicCrawler extends Crawler {
       case "R&B/Soul" : appendURI = melonGenreURI + "0400";  break;
       case "POP" : appendURI = melonGenreURI + "0900";  break;
     }
-    Document doc = this.getDoc(melonURL + appendURI);
+
+    // DOM Data를 가져온 후
+    Document doc = getDoc(melonURL + appendURI);
+
+    /*
+      DOM에서 음원 목록에 해당하는 부분을 선택한다.
+      lst class는 실시간 top 100에만 존재한다.
+      lst class가 없다면 그냥 tbody의 tr을 가져오면 된다.
+     */
     Elements els = doc.select("tr[class^=\"lst\"]");
     if (els.size() == 0) {
       els = doc.select("tbody tr");
     }
+
+    // 크롤링한 데이터를 MusicArticle에 Mapping 후 List 형태로 반환한다.
     List<MusicArticle> articles = new ArrayList();
     els.forEach(el -> {
       articles.add(
--- server/src/main/java/zuminternet/pilot/helper/crawler/NewsCrawler.java
+++ server/src/main/java/zuminternet/pilot/helper/crawler/NewsCrawler.java
@@ -12,17 +12,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class NewsCrawler extends Crawler {
+public class NewsCrawler extends Crawler { // Crawler Class를 Extends함 --> getDoc Method 사용 가능
 
   @Value("${url.sbs}")
-  private String sbsURL;
+  private String sbsURL; // application.yml의 url.sbs를 가져옴(SBS K-POP NEWS URL)
 
   @Value("${url.billboard}")
-  private String billboardURL;
+  private String billboardURL; // applicatino.yml의 url.billboard를 가져옴
 
+  /**
+   * SBS K-POP NEWS의 목록을 가져온다.
+   * @param page pagination number. 한 페이지에 20개의 기사가 존재한다.
+   * @return 크롤링 해온 데이터를 NewsArticle에 Mapping 후 List 형태로 반환
+   */
   public List<NewsArticle> getList (int page) {
+    // SBS NEWS Page에서 DOM 정보를 가져온다.
     Document doc = getDoc(sbsURL + "/news/ssports_list.jsp?code_category=SS04&pageNo=" + page);
+
+    // 기사 목록들에 대한 태그를 선택
     Elements els = doc.select(".list_news").select("li");
+
+    // mapping
     List<NewsArticle> articles = new ArrayList();
     els.forEach(el -> {
       articles.add(
@@ -36,6 +46,10 @@ public class NewsCrawler extends Crawler {
     return articles;
   }
 
+  /**
+   * SBS NEWS의 Headline 기사를 가져온다.
+   * @return NewsArticle에 Mapping 후 반환
+   */
   public NewsArticle getHeadline () {
     Elements el = getDoc(sbsURL + "/news").select(".hline_hot");
     return NewsArticle.builder()
@@ -45,9 +59,18 @@ public class NewsCrawler extends Crawler {
             .link(el.select("a").attr("href")).build();
   }
 
+  /**
+   * Billboard k-pop news의 상단 4개의 기사를 가져온다.
+   * @return NewArticle에 mapping후 List 형태로 반환한다.
+   */
   public List<NewsArticle> getPopular () {
+    // DOM 정보를 가져옴
     Document doc = getDoc(billboardURL + "/main/news/list");
+
+    // 상단 4개의 기사 선택
     Elements els = doc.select("#new_area").select("li:nth-child(1), li:nth-child(2), li:nth-child(3), li:nth-child(4)");
+
+    // 맵핑
     List<NewsArticle> articles = new ArrayList();
     els.forEach(el -> {
       articles.add(
@@ -61,16 +84,40 @@ public class NewsCrawler extends Crawler {
     return articles;
   }
 
+  /**
+   * URL에 해당하는 News Content 정보를 가져온다.
+   * @param url : 가져올 Content의 URL
+   * @return : SBS NEWS와 Billboard NEWS를 판별하여 필요없는 태그를 지운 후 NewsArticle에 mapping 하여 반환한다.
+   */
   public NewsArticle getContent (String url) {
+    // DOM 정보를 가져옴
     Document doc = getDoc(url);
+
+    // SBS NEWS -> true, Billboard News -> false
     Boolean bool = url.indexOf(sbsURL) != -1;
+
+    // bool의 값을 통하여 어떤 tag의 내용을 가져올지 선택
     Elements el = doc.select(bool ? "#etv_news_content" : ".text");
+
+    // sbs news의 경우 data-v_src를 src에 맵핑
+    // sbs news는 초기 로딩을 빠르게 하기 위하여 이미지의 src를 뒤늦게 load하는 형태
     if (bool) {
       el.select("img").forEach(v -> {
         v.attr("src", v.attr("data-v_src"));
       });
     }
+
+    /*
+      참고 : https://offbyone.tistory.com/375
+      Contnet의 내용을 Jsoup 에서 제공하는 whitelist 방식으로 tag를 검열한다.
+      허용하는 tag는 다음과 같다.
+      a, b, blockquote, br, caption, cite, code, col, colgroup, dd, div, dl, dt, em, h1, h2, h3, h4, h5, h6,
+      i, img, li, ol, p, pre, q, small, span, strike, strong, sub, sup,
+      table, tbody, td, tfoot, th, thead, tr, u, ul
+    */
     String cleanContent = Jsoup.clean(el.html(), Whitelist.relaxed());
+
+    // 제목과 컨텐츠만 맵핑하여 반환한다.
     return NewsArticle.builder()
             .title(doc.select(bool ? "h1" : ".ttl.tac .big").text())
             .content(cleanContent).build();
