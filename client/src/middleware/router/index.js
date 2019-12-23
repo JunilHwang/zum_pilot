7f2b107fcb34ad02a359cfb6930756800bf7a59d
junilhwang <tjsdlf4261@naver.com>
2019-12-23 15:14:12 +0900

    Router 코드 개선

--- client/src/middleware/router/index.js
+++ client/src/middleware/router/index.js
@@ -1,46 +1,18 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import {
-  News,
-  Chart,
-  Login,
-  Join,
-  Popular,
-  Bookmark,
-} from '@/views';
+import { News, Chart, Login, Join, Popular, Bookmark } from '@/views';
 
 Vue.use(VueRouter);
-const routes = [
-  {
-    path: '/',
-    alias: '/news',
-    component: News,
-  },
-  {
-    path: '/chart',
-    component: Chart,
-  },
-  {
-    path: '/sign-in',
-    component: Login,
-  },
-  {
-    path: '/sign-up',
-    component: Join,
-  },
-  {
-    path: '/popular',
-    component: Popular,
-  },
-  {
-    path: '/bookmark',
-    component: Bookmark,
-  },
-];
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
-});
 
-export default router;
+const mode = 'history';
+const base = process.env.BASE_URL;
+const routes = [
+  { path: '/', component: News, alias: '/news' },
+  { path: '/chart', component: Chart },
+  { path: '/sign-in', component: Login },
+  { path: '/sign-up', component: Join },
+  { path: '/popular', component: Popular },
+  { path: '/bookmark', component: Bookmark },
+];
+
+export default new VueRouter({ mode, base, routes });
