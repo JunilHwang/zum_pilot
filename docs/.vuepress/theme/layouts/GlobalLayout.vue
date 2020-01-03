3706384ad9b621a6181fa19ccf746024ba25aff3
junilhwang <tjsdlf4261@naver.com>
2020-01-03 15:24:06 +0900

    Vuepress Theme Extend 완료

---
+++ docs/.vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="global-layout">
+    <Layout />
+    <footer class="siteFooter">
+      <div class="container">
+        <p class="copyright">
+          Copyright &copy; 2019 <strong>zuminternet corp.</strong> All Right Reserved
+        </p>
+      </div>
+    </footer>
+  </div>
+</template>
