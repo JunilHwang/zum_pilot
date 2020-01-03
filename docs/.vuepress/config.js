0293e041ab8561159de78887cbe8267f50bbe863
junilhwang <tjsdlf4261@naver.com>
2020-01-03 14:44:31 +0900

    기술ë¬vuepress로 기술문서 세팅 완료

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '주제별 영상제공 웹 서비스',
+  description: 'Zuminternet Pilot Project',
+  themeConfig: {
+    search: false,
+    logo: '/img/logo.gif',
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: 'auto'
+  }
+}
