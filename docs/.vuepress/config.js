53bc7f5166333913ef01cf27671d991b69ae7170
junilhwang <tjsdlf4261@naver.com>
2020-01-06 08:13:45 +0900

    문서  UI 수정

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,18 +3,16 @@ module.exports = {
   title: '주제별 영상제공 웹 서비스',
   description: 'Zuminternet Pilot Project',
   themeConfig: {
-    search: false,
     logo: '/img/logo.gif',
-    nav: [
-      { text: '개요', link: '/' },
-      { text: '시나리오', link: '/posts/Scenario' },
-      { text: '아키텍쳐', link: '/posts/Architecture' },
-      { text: '설계', link: '/posts/Design' },
-      { text: 'Server', link: '/posts/Server-side' },
-      { text: 'Client', link: '/posts/Client-side' },
-      { text: 'Reference', link: '/posts/Reference' },
+    sidebar: [
+      { title: '개요', path: '/' },
+      { title: '시나리오', path: '/Scenario' },
+      { title: '아키텍쳐', path: '/Architecture' },
+      { title: '설계', path: '/Design' },
+      { title: 'Server', path: '/Server-side' },
+      { title: 'Client', path: '/Client-side' },
+      { title: 'Reference', path: '/Reference' },
     ],
-    sidebar: 'auto',
     smoothScroll: true
   },
   base: '/zum_pilot/',
